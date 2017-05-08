de896c316333d2ea04b0879ace5fb36db2eb7ce6
Felipe Frizzo <felipefrizzo@gmail.com>
2017-05-08 19:11:39 -0300

    Start subscription view

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,15 +6,21 @@
     .controller('MainController', MainController);
 
   MainController.$inject = [];
-  
+
   /** @ngInject */
   function MainController() {
     var vm = this;
+    vm.year = new Date().getFullYear();
+    vm.sendSubscription = sendSubscription;
 
     activate();
 
     function activate() {
 
+    }
+
+    function sendSubscription(subscription) {
+      console.log(subscription);
     }
   }
 })();
