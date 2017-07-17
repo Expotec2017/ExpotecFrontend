114184a062bb3a8416676f94632638eb90487708
Felipe Frizzo <felipefrizzo@gmail.com>
2017-07-17 09:07:01 -0300

    update

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -3,6 +3,6 @@
 
   angular
     .module('expotec')
-    .constant('BASE_URL', { URL: "http://localhost:8000/api/web" });
-    // .constant('BASE_URL', { URL: "http://187.19.101.152:8000/api/web" });
+    // .constant('BASE_URL', { URL: "http://localhost:8000/api/web" });
+    .constant('BASE_URL', { URL: "http://187.19.101.152:8080/api/web" });
 })();
